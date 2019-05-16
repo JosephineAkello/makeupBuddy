c7d69dd2e1c79697215d35d42e17fad1e27a69a2
mj <onyangomaureen95@gmail.com>
2019-05-16 09:02:36 +0300

    add a scaffold and import home file

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 class App extends StatelessWidget{
   Widget build(context){
-  return MaterialApp();
+  return MaterialApp(
+    theme: ThemeData(primaryColor: Colors.indigo),
+    title: 'Makeup budyy',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Makeup Buddy'),
+      ),
+      body: Home(),
+    ),
+  );
 }
 }
