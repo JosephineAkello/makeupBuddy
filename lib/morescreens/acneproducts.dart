a5710413226505f6750809f105822eb973f4ef12
mj <onyangomaureen95@gmail.com>
2019-05-22 11:57:30 +0300

    create acne products class

---
+++ lib/morescreens/acneproducts.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AcneProducts extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Acne skincare products'),
+        backgroundColor: Colors.lightBlue[100],
+      ),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
