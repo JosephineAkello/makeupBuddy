87d07140058d18fbdfe1c08edff85df4f2d43f10
mj <onyangomaureen95@gmail.com>
2019-05-22 10:07:04 +0300

    format document

--- lib/morescreens/eyebrows.dart
+++ lib/morescreens/eyebrows.dart
@@ -51,7 +51,7 @@ class Eyebrows extends StatelessWidget {
                     'While waxing,a warm wax is thinly applied with a wooden spatula to the hairs that will be removed, a paper strip is then applied directly over the area with the wax, gentle pressure is applied to ensure the hairs in the wax stick to the paper, the paper is then swiftly pulled away, pulling the hairs out directly from the root. '),
                 RaisedButton(
                   shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
+                      borderRadius: BorderRadius.circular(20.0)),
                   onPressed: () {},
                   child: Text(
                     'Learn more..',
@@ -66,29 +66,38 @@ class Eyebrows extends StatelessWidget {
             ),
           ),
           Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
-            color: Colors.lightBlue[100],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              color: Colors.lightBlue[100],
               child: ListTile(
-            title: Text('3. Eyebrows shaping kits', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
-            subtitle: Column(
-              children: <Widget>[
-                
-            Image.asset('assets/shapingkit.jpg'),
-            Text('Check these amazing eyebrow shaping kits, some of them are stensils and brow Zings'),
-              ],
-            ),
-          )),
+                title: Text(
+                  '3. Eyebrows shaping kits',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  children: <Widget>[
+                    Image.asset('assets/shapingkit.jpg'),
+                    Text(
+                        'Check these amazing eyebrow shaping kits, some of them are stensils and brow Zings'),
+                  ],
+                ),
+              )),
           Card(
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20.0)),
             child: FlatButton.icon(
               onPressed: () {
-                 Navigator.push(context,
+                Navigator.push(context,
                     MaterialPageRoute(builder: (context) => EyebrowShape()));
               },
               icon: Icon(Icons.navigate_next),
-              label: Text('Check out how to shape eyebrows', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.blue),),
+              label: Text(
+                'Check out how to shape eyebrows',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue),
+              ),
             ),
           )
         ],
