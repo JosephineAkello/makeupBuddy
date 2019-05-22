d5fe25b4ccfcd4c8592b7838e7f90f68a284899b
mj <onyangomaureen95@gmail.com>
2019-05-22 19:20:00 +0300

    place images in a gridview

--- lib/morescreens/acneproducts.dart
+++ lib/morescreens/acneproducts.dart
@@ -7,42 +7,48 @@ class AcneProducts extends StatelessWidget {
         title: Text('Acne skincare products'),
         backgroundColor: Colors.lightBlue[100],
       ),
-      body: Column(
-        children: [
-          Image.asset('assets/afteracnes.png'),
-          Text(
-              'Everyone desires a flawless skin, here are some of the products that you might use'),
-          GridView.count(
-            crossAxisCount: 2,
-            children: <Widget>[
-              GridTile(
-                header: Image.asset('assets/cetaphil.jpg'),
-                child: Text('Cetaphyl'),
-              ),
-              GridTile(
-                header: Image.asset('assets/acnemosturizer.jpg'),
-                child: Text('Mosturizer'),
-              ),
-              GridTile(
-                header: Image.asset('assets/gyloyc.jpg'),
-                child: Text('Glycollic'),
-              ),
-              GridTile(
-                header: Image.asset('assets/benzovyl.jpg'),
-                child: Text('Benzoyl peroxide'),
-              ),
-              GridTile(
-                header: Image.asset('assets/dettoleventon.jpg'),
-                child: Text('Dettol eventone'),
-              ),
-              GridTile(
-                header: Image.asset('assets/duduosun.jpg'),
-                child: Text('Dudu Osun soap'),
-              )
-            ],
-          )
+      body: InkWell(
+          child: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          Card(
+            child: GridTile(
+              child: Image.asset('assets/cetaphil.jpg'),
+              footer: Text('Cetaphyl'),
+            ),
+          ),
+          Card(
+            child: GridTile(
+              child: Image.asset('assets/acnemosturizer.jpg'),
+              footer: Text('Mosturizer'),
+            ),
+          ),
+          Card(
+            child: GridTile(
+              child: Image.asset('assets/gyloyc.jpg'),
+              footer: Text('Glycollic'),
+            ),
+          ),
+          Card(
+            child: GridTile(
+              child: Image.asset('assets/benzovyl.jpg'),
+              footer: Text('Benzoyl peroxide'),
+            ),
+          ),
+          Card(
+            child: GridTile(
+              child: Image.asset('assets/dettoleventone.jpg'),
+              footer: Text('Dettol eventone'),
+            ),
+          ),
+          Card(
+            child: GridTile(
+              child: Image.asset('assets/duduosun.jpg'),
+              footer: Text('Dudu Osun soap'),
+            ),
+          ),
         ],
-      ),
+      )),
     );
   }
 }
