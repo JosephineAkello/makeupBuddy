529249ad1a1c2d04c41ed3f6e48af4e816a0fb0f
mj <onyangomaureen95@gmail.com>
2019-05-23 10:28:32 +0300

    center the video player

--- lib/morescreens/foundationVideos.dart
+++ lib/morescreens/foundationVideos.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player/youtube_player.dart';
- 
- class FoundationVideos extends StatelessWidget{
-   VideoPlayerController _controller;
 
-   Widget build(BuildContext context) {
-     return  Scaffold(
-       appBar: AppBar(
-         title: Text('Practical video example'),
-         backgroundColor: Colors.lightBlue[100],
-       ),
-       body: Container(
-         child: Column(
-           children: [
-           YoutubePlayer(
-             context: context,
-             source: ' https://www.youtube.com/watch?v=DsW19ibcHII',
-             quality: YoutubeQuality.MEDIUM,
-             callbackController: (controller){
-               controller = _controller;
-             },
-             )
-         ])
-       ),
-     ) ;
-   }
- }
+class FoundationVideos extends StatefulWidget {
+  createState() {
+    return FoundationVideosState();
+  }
+}
+
+class FoundationVideosState extends State<FoundationVideos> {
+  VideoPlayerController _controller;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practical video example'),
+        backgroundColor: Colors.lightBlue[100],
+      ),
+      body: Center(
+        child: YoutubePlayer(
+          context: context,
+          source: ' https://www.youtube.com/watch?v=DsW19ibcHII',
+          quality: YoutubeQuality.MEDIUM,
+          callbackController: (controller) {
+            _controller = controller;
+          },
+        ),
+      ),
+    );
+  }
+}
