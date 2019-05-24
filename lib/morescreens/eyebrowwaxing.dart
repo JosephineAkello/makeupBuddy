0b85a17e60e7de3a782c9e3995bd8f7b76edb09f
mj <onyangomaureen95@gmail.com>
2019-05-24 09:10:07 +0300

    place the youtube widget with source url

--- lib/morescreens/eyebrowwaxing.dart
+++ lib/morescreens/eyebrowwaxing.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player/youtube_player.dart';
 
 class EyebrowWaxing extends StatelessWidget{
   Widget build(context){
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Eyebrow waxing tutorial'),
+        backgroundColor: Colors.lightBlue[100],
+      ),
+      body: Center(
+        child: YoutubePlayer(
+          context: context,
+          source: 'https://www.youtube.com/watch?v=WYmuk3UVSTQ',
+          quality: YoutubeQuality.MEDIUM,
+        ),
+      ),
     );
   }
 }
