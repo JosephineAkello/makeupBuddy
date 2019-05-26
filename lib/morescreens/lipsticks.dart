d78468c18a155f9d343a9f5248c92e11f0033c2a
mj <onyangomaureen95@gmail.com>
2019-05-26 16:43:33 +0300

    create list of lipsticks items

--- lib/morescreens/lipsticks.dart
+++ lib/morescreens/lipsticks.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Lipsticks extends StatelessWidget{
-  Widget build(context){
+class Lipsticks extends StatelessWidget {
+  Widget build(context) {
     return Scaffold(
-      
-    );
+        appBar: AppBar(
+          title: Text('Lipsticks'),
+          backgroundColor: Colors.lightBlue[100],
+        ),
+        body: Container(
+            margin: EdgeInsets.symmetric(vertical: 20.0),
+            child: ListView(scrollDirection: Axis.horizontal, children: [
+              Card(
+                child: ListTile(
+                  title: Image.asset('assets/lipstickstypes.jpg'),
+                  subtitle: Text('data'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Image.asset('assets/brightlipsticks.jpg'),
+                  subtitle: Text('data'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Image.asset('assets/nudelipstickstpe.jpg'),
+                  subtitle: Text('data'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Image.asset('assets/manyweird.jpg'),
+                  subtitle: Text('data'),
+                ),
+              ),
+
+             
+            ])));
   }
 }
