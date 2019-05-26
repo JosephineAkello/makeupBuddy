dfb779881a0e5fb5e3565fdd89795c6c1f9a0578
mj <onyangomaureen95@gmail.com>
2019-05-26 23:19:34 +0300

    add shades of lipsticks vertical list

--- lib/morescreens/lipsticks.dart
+++ lib/morescreens/lipsticks.dart
@@ -7,35 +7,102 @@ class Lipsticks extends StatelessWidget {
           title: Text('Lipsticks'),
           backgroundColor: Colors.lightBlue[100],
         ),
-        body: Container(
-            margin: EdgeInsets.symmetric(vertical: 20.0),
-            child: ListView(scrollDirection: Axis.horizontal, children: [
-              Card(
-                child: ListTile(
-                  title: Image.asset('assets/lipstickstypes.jpg'),
-                  subtitle: Text('data'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Image.asset('assets/brightlipsticks.jpg'),
-                  subtitle: Text('data'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Image.asset('assets/nudelipstickstpe.jpg'),
-                  subtitle: Text('data'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Image.asset('assets/manyweird.jpg'),
-                  subtitle: Text('data'),
-                ),
-              ),
-
-             
-            ])));
+        body: SingleChildScrollView(
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+          SizedBox(
+            height: 200,
+            child: ListView(
+                //  physics: ClampingScrollPhysics(),
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.only(
+                    bottom: 20.0, top: 20.0, left: 20.0, right: 20.0),
+                children: [
+                  Image.asset('assets/lipstickstypes.jpg'),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Image.asset('assets/brightlipsticks.jpg'),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Image.asset('assets/nudelipstickstpe.jpg'),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Image.asset('assets/manyweird.jpg'),
+                ]),
+          ),
+          Text(
+            'Shades of Lipsticks',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/rosepink.jpg'),
+              subtitle: Text('Rose pink lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/copperbrown.jpg'),
+              subtitle: Text('Copper brown'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/taupelipsticks.jpg'),
+              subtitle: Text('Taupe lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/fuschialipsticks.jpg'),
+              subtitle: Text('Fuschia Lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/chocolatebrown.png'),
+              subtitle: Text('ChocolateBrown lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/peachlipsticks.jpg'),
+              subtitle: Text('Peach Lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/bronzelipsticks.jpg'),
+              subtitle: Text('Bronze Lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/magentalipsticks.jpg'),
+              subtitle: Text('Magenta Lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/nudelipsticks.jpeg'),
+              subtitle: Text('Nude Lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/redlipsticks.jpg'),
+              subtitle: Text('Red Lipsticks'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Image.asset('assets/funnylips.jpg'),
+              subtitle: Text('Crazy look'),
+            ),
+          ),
+        ])));
   }
 }
