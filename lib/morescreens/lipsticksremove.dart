cc9eb9a16f83b64606f2f7343439b99aef01aab6
mj <onyangomaureen95@gmail.com>
2019-05-27 22:42:25 +0300

    create how to remove lipsticks page

---
+++ lib/morescreens/lipsticksremove.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RemoveLipsticks extends StatelessWidget{
+  Widget build(context){
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('How to remove lipsticks'),
+      backgroundColor: Colors.lightBlue[100],
+    ),
+  );
+  }
+}
