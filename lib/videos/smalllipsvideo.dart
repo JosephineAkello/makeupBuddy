2078cf6d84a567be43596f83d8982df77226878e
mj <onyangomaureen95@gmail.com>
2019-05-27 18:13:42 +0300

    change class to stateful

--- lib/videos/smalllipsvideo.dart
+++ lib/videos/smalllipsvideo.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_player/youtube_player.dart';
 
-class SmallLipsVideo extends StatelessWidget{
-  Widget build(context){
+class SmallLipsVideo extends StatefulWidget {
+  createState() {
+    return SmallLipsVideoState();
+  }
+}
+
+class SmallLipsVideoState extends State<SmallLipsVideo> {
+  VideoPlayerController _controller;
+
+  Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Watch video tutorial'),
@@ -12,7 +20,10 @@ class SmallLipsVideo extends StatelessWidget{
         child: YoutubePlayer(
           context: context,
           source: 'https://youtu.be/en8Tdd4sA-A',
-         quality: YoutubeQuality.MEDIUM,
+          quality: YoutubeQuality.MEDIUM,
+          callbackController: (controller) {
+            _controller = controller;
+          },
         ),
       ),
     );
