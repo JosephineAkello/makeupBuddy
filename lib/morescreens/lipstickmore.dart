17b79ed4bbe0625c03e5bb0c8f40da0229bcc0ae
mj <onyangomaureen95@gmail.com>
2019-05-27 09:57:33 +0300

    create a class to give details about lipsticks

---
+++ lib/morescreens/lipstickmore.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Morelipsticks extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('More insights on Lipsticks'),
+        backgroundColor: Colors.lightBlue[100],
+      ),
+      body: Container(),
+    );
+  }
+}
