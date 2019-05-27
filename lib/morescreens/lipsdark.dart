57bba4334f3cf579b901f96834c045746c4135c7
mj <onyangomaureen95@gmail.com>
2019-05-27 18:19:28 +0300

    create darklips page

---
+++ lib/morescreens/lipsdark.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DarkLips extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Applying Lipsticks on Dark lips'),
+      ),
+    );
+  }
+}
