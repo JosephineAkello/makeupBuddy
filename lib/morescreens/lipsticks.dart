283459637fede56e3bce6de7a4eaa9956f27dc70
mj <onyangomaureen95@gmail.com>
2019-05-28 15:16:57 +0300

    format document

--- lib/morescreens/lipsticks.dart
+++ lib/morescreens/lipsticks.dart
@@ -11,8 +11,9 @@ class Lipsticks extends StatelessWidget {
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.navigate_next),
           backgroundColor: Colors.purple,
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> Morelipsticks()));
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => Morelipsticks()));
           },
         ),
         body: SingleChildScrollView(
@@ -46,178 +47,211 @@ class Lipsticks extends StatelessWidget {
             style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/rosepink.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Rose pink lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/rosepink.jpg'),
+                Container(
+                  color: Colors.white30,
+                  child: ListTile(
+                    title: Text(
+                      'Rose pink lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        ' This shade of lipstick looks good on anyone with any skin tone'),
                   ),
-                  Text(
-                      ' This shade of lipstick looks good on anyone with any skin tone'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/copperbrown.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Copper brown',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/copperbrown.jpg'),
+                Container(
+                  color: Colors.white12,
+                  child: ListTile(
+                    title: Text(
+                      'Copper brown',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'Any skin tone can apply this but it looks appealing on dark women.'),
                   ),
-                  Text(
-                      'Any skin tone can apply this but it looks appealing on dark women.'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/taupelipsticks.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Taupe lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/taupelipsticks.jpg'),
+                Container(
+                  color: Colors.white12,
+                  child: ListTile(
+                    title: Text(
+                      'Taupe lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'Those with lightly deeper complexion can apply this, looks good on them'),
                   ),
-                  Text(
-                      'Those with lightly deeper complexion can apply this, looks good on them'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/fuschialipsticks.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Fuschia Lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/fuschialipsticks.jpg'),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'Fuschia Lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'This shade is all inclusive, however those worried about pink lips can use this shade'),
                   ),
-                  Text(
-                      'This shade is all inclusive, however those worried about pink lips can use this shade'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/chocolatebrown.png'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'ChocolateBrown lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/chocolatebrown.png'),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'ChocolateBrown lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'This shade looks natural on dark skinned color, it gives a sophisticated look'),
                   ),
-                  Text(
-                      'This shade looks natural on dark skinned color, it gives a sophisticated look'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/peachlipsticks.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Peach Lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/peachlipsticks.jpg'),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'Peach Lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'This shade looks awesome on fair skinned people to complement their skin tone'),
                   ),
-                  Text(
-                      'This shade looks awesome on fair skinned people to complement their skin tone'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/bronzelipsticks.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Bronze Lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/bronzelipsticks.jpg'),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'Bronze Lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'Looks elegant and its not shouting, any skintone can apply this shade'),
                   ),
-                  Text(
-                      'Looks elegant and its not shouting, any skintone can apply this shade'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/magentalipsticks.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Magenta Lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/magentalipsticks.jpg'),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'Magenta Lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text('Looks marvelous in all skin shades,'),
                   ),
-                  Text('Looks marvelous in all skin shades,'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/nudelipsticks.jpeg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Nude Lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/nudelipsticks.jpeg'),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'Nude Lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'If you are for simple, formal and soft look, you can go to nude lipsticks shade.'),
                   ),
-                  Text(
-                      'If you are for simple, formal and soft look, you can go to nude lipsticks shade.'),
-                ],
-              ),
+                )
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/redlipsticks.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Red Lipsticks',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/redlipsticks.jpg'),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'Red Lipsticks',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'Looks enticingly hot and seductive on dark women. However, those with fair skin can still apply.'),
                   ),
-                  Text(
-                      'Looks enticingly hot and seductive on dark women. However, those with fair skin can still apply.'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
           Card(
-            child: ListTile(
-              title: Image.asset('assets/funnylips.jpg'),
-              subtitle: Column(
-                children: [
-                  Text(
-                    'Crazy look',
-                    style:
-                        TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Image.asset('assets/funnylips.jpg'),
+                Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'Crazy look',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                        'If you want a crazy look, you can apply these sets of shades, to give the creative apperance. This can be relevant during modelling competitions.'),
                   ),
-                  Text(
-                      'If you want a crazy look, you can apply these sets of shades, to give the creative apperance. This can be relevant during modelling competitions.'),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ])));
