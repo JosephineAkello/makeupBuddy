1d88e3c6de7ee59d6817ee221757762457f3e811
mj <onyangomaureen95@gmail.com>
2019-05-29 09:01:17 +0300

    create applying mascara naturally page

---
+++ lib/morescreens/mascaranatural.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NaturalMascara extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Applying Mascara with Natural eyelashes'),
+      ),
+      body: Container(),
+    );
+  }
+}
