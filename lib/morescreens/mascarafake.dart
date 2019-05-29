ca4ecf5b402ab2f25ea9439e45d4d9ad4c1c1e18
mj <onyangomaureen95@gmail.com>
2019-05-29 09:02:04 +0300

    create applying fake mascara page

---
+++ lib/morescreens/mascarafake.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FakeMascara extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Applying mascara with Fake eyelashes'),
+        backgroundColor: Colors.lightBlue[100],
+      ),
+      body: Container(),
+    );
+  }
+}
