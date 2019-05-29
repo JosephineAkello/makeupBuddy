c9033461e5d63f678a92729879adb50a0e943682
mj <onyangomaureen95@gmail.com>
2019-05-29 09:00:22 +0300

    create mascara tools page

---
+++ lib/morescreens/mascaratools.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MascaraTools extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Applying Mascara with Natural eyelashes'),
+      ),
+      body: Container(),
+    );
+  }
+}
