18742f5462844dd7d5ce9d8938cee6857d2e067b
mj <onyangomaureen95@gmail.com>
2019-05-30 11:01:12 +0300

    craete nails couture page

---
+++ lib/morescreens/nails.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class Nails extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nails Couture'),
+        backgroundColor: Colors.lightBlue[100],
+      ),
+      body: SingleChildScrollView(),
+    );
+  }
+}
