463b34e0e3e5916b9756726c9c85fc98bcc22ff9
mj <onyangomaureen95@gmail.com>
2019-05-30 15:47:09 +0300

    create nailtools page

---
+++ lib/morescreens/nailtools.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NailTools extends StatelessWidget{
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nail Tools'),
+        backgroundColor: Colors.lightBlue[100],
+      ),
+      body: Container(),
+    );
+  }
+}
