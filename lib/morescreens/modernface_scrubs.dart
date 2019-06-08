533d20a5a62915f8ee2b8d8b0dce3ce3ec89b24d
mj <onyangomaureen95@gmail.com>
2019-06-08 13:46:54 +0300

    create a new offshelf scrubs page

---
+++ lib/morescreens/modernface_scrubs.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ModernScrubs extends StatelessWidget{
+  Widget build(context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Lipsticks'),
+          backgroundColor: Colors.lightBlue[100],
+        ),
+        body: Container(),
+         );
+  
+  }
+}
