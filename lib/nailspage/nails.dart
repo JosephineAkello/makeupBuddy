bede5e3b7b1759b2de1fd4d64f36d5d214b3bdd7
mj <onyangomaureen95@gmail.com>
2019-06-16 08:18:09 +0300

    change to stateful and provide variable naillists

--- lib/nailspage/nails.dart
+++ lib/nailspage/nails.dart
@@ -1,12 +1,117 @@
 import 'package:flutter/material.dart';
 import 'nailtools.dart';
 
-class Nails extends StatelessWidget {
+class Nails extends StatefulWidget {
+  createState() {
+    return NailState();
+  }
+}
+
+class NailState extends State<Nails> {
+  var nailLists = [
+    {
+      "name": "Glitters shade",
+      "image": "assets/glitters.jpg",
+      "description": "buggy pants for women",
+      "price": 2000,
+    },
+    {
+      "name": "Green shade",
+      "image": "assets/greenshade.jpg",
+      "description": "buggy pants for women",
+      "price": 1000,
+    },
+    {
+      "name": "Glaze nails",
+      "image": "assets/glazenails.jpg",
+      "description": "buggy pants for women",
+      "price": 1500,
+    },
+    {
+      "name": "Midnight blue",
+      "image": "assets/midnightblue.jpg",
+      "description": "buggy pants for women",
+      "price": 1000,
+    },
+    {
+      "name": "Yellow nails",
+      "image": "assets/yellownails.jpg",
+      "description": "buggy pants for women",
+      "price": 2000,
+    },
+    {
+      "name": "Milky white",
+      "image": "assets/milkywhite.jpg",
+      "description": "buggy pants for women",
+      "price": 2500,
+    },
+    {
+      "name": "Dark peach",
+      "image": "assets/darkpeach.jpg",
+      "description": "buggy pants for women",
+      "price": 1200,
+    },
+    {
+      "name": "Dark Blue",
+      "image": "assets/darkblue.jpg",
+      "description": "buggy pants for women",
+      "price": 3000,
+    },
+    {
+      "name": "Brown shade",
+      "image": "assets/brown.jpg",
+      "description": "buggy pants for women",
+      "price": 2000,
+    },
+    {
+      "name": "Black shade",
+      "image": "assets/blacknails.jpg",
+      "description": "buggy pants for women",
+      "price": 1000,
+    },
+    {
+      "name": "3D acrylic shade",
+      "image": "assets/3dacrylic.jpg",
+      "description": "buggy pants for women",
+      "price": 1500,
+    },
+    {
+      "name": "White shade",
+      "image": "assets/whitenails.jpg",
+      "description": "buggy pants for women",
+      "price": 1000,
+    },
+    {
+      "name": "Purple shade",
+      "image": "assets/purple.jpg",
+      "description": "buggy pants for women",
+      "price": 2000,
+    },
+    {
+      "name": "Sky blue shade",
+      "image": "assets/skyblue.jpg",
+      "description": "buggy pants for women",
+      "price": 2500,
+    },
+    {
+      "name": "Red shade",
+      "image": "assets/rednails.jpg",
+      "description": "buggy pants for women",
+      "price": 1200,
+    },
+    {
+      "name": "Quilted",
+      "image": "assets/quiltednails.jpg",
+      "description": "buggy pants for women",
+      "price": 3000,
+    },
+  ];
+
   Widget build(context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Nail polish Shades'),
-          backgroundColor: Colors.pink, 
+          backgroundColor: Colors.pink,
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.purple,
