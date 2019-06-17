d1e62e548a11133ab7d1e1971ad6a98b874e3e97
mj <onyangomaureen95@gmail.com>
2019-06-17 11:43:01 +0300

    provide nail details description

--- lib/nailspage/naildetails.dart
+++ lib/nailspage/naildetails.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
 
-class NailDetails extends StatefulWidget{
-createState(){
-  return NailDetailsState();
-}
-}  
+class NailDetails extends StatefulWidget {
+  final naildetailsname;
+  final naildetailsimage;
+  final naildetailsdescription;
 
-class NailDetailsState extends State<NailDetails>{
-  Widget build(context){
+  NailDetails({
+    this.naildetailsname,
+    this.naildetailsimage,
+    this.naildetailsdescription,
+  });
+
+  createState() {
+    return NailDetailsState();
+  }
+}
+
+class NailDetailsState extends State<NailDetails> {
+  Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nail shades description'),
+        title: Text('Nail shades details'),
       ),
-      body: Container(),
+      body: ListView(children: [
+        Container(
+            height: 300,
+            child: GridTile(
+                child: Container(
+                    child: Image.asset(
+                  widget.naildetailsimage,
+                )),
+                footer: Container(
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Text(
+                      widget.naildetailsname,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                          
+                    ),
+                  trailing: Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                            )
+                  ),
+                ))),
+        Padding(
+          padding: EdgeInsets.all(18.0),
+          child:Text(
+          widget.naildetailsdescription,style: TextStyle(fontSize: 18.0),
+        ),),
+      ]),
     );
   }
 }
